07a2c555000f03d67ebf3c9e9d5ecb080dea291e
miguel17546mals <miguel17546mals@gmail.com>
2019-09-16 22:02:15 -0500

    [ADD] P3VLSI codigo funcional para la practica 3

--- Practica_03/P3VLSI.vhd
+++ Practica_03/P3VLSI.vhd
@@ -1,3 +1,4 @@
+
 ---------------------------------------------------------------------- 
 -- Practica 3. Generación de Frecuencia, Relojes y Temporizadores. 
 -- Module Name:    divisor - Behavioral  
@@ -11,27 +12,30 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL; 
 use IEEE.STD_LOGIC_ARITH.ALL; 
 use IEEE.STD_LOGIC_UNSIGNED.ALL; 
+use ieee.numeric_std.all;
 
-entity P3VLSI is    
+entity P3 is    
 Port ( clk     : in std_logic;
 		div_clk : out std_logic); 
-end P3VLSI; 
+end P3; 
  
-architecture Behavioral of P3VLSI is 
+architecture Behavioral of P3 is 
 begin    
 	process (clk)
 		constant fclk : integer := 50_000_000;
 		constant f : integer := 1;
 		constant CT : integer:= 30;
 		
-		variable cuenta:  := 0;   
+		variable cuenta: integer  := 0;   
+		variable resultado: integer :=0;
 		
 		begin 
       
 		 if rising_edge (clk) then          
 			 if cuenta < (fclk/f -1) then             
 				 cuenta := cuenta + 1;
-				 if cuenta < (fclk/f -1) * (CT/100) then
+				 resultado := (fclk)/(100*f/CT) ;
+				 if cuenta < resultado  then
 					div_clk <= '1';
 				 else 
 				   div_clk <= '0';
